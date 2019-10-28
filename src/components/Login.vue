c8474d5f4c4233767b80898f020485a35479c67d
Laetitia <laetitiabodinier@gmail.com>
2019-10-28 15:19:01 +0100

    reset

---
+++ src/components/Login.vue
@@ -0,0 +1,103 @@
+<template>
+<v-container>
+    <v-layout text-center wrap>
+      <v-flex>
+        <v-form>
+          <v-alert dense dismissible v-model="error" type="error">{{alertmsg}}</v-alert>
+          <v-alert dense dismissible v-model="warning" type="warning">{{alertmsg}}</v-alert>
+          <v-alert dense dismissible v-model="success" type="success">{{alertmsg}}</v-alert>
+          <v-container>
+            <v-row>
+              <v-col>
+                <v-text-field v-model="Name" label="Name" required></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field v-model="Password" label="Password" type='password' required></v-text-field>
+              </v-col>
+            </v-row>
+            <v-btn @click="login()">Log in</v-btn>
+            <v-spacer></v-spacer>
+            <br/>
+            <v-btn @click="register">Register</v-btn>
+          </v-container>
+        </v-form>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    Name: '',
+    Password: '',
+    error: false,
+    warning: false,
+    success: false,
+    alertmsg: ''
+  }),
+  methods: {
+    async login () {
+      if (this.Password === '' || this.Name === '') {
+        // console.log('missing password or name')
+        this.alertmsg = 'Password or name is empty'
+        this.warning = true
+      } else {
+        if (this.$session.id()) {
+          this.alertmsg = 'A user is already connected'
+          this.warning = true
+        } else {
+          // console.log('connexion request')
+          const log = await this.axios.post('/api/login', {
+            name: this.Name,
+            password: this.Password
+          })
+          // console.log(log.data.status)
+          if (!log.data.status) {
+            // console.log('wrong name or wrong password')
+            this.alertmsg = 'Password or name is wrong'
+            this.error = true
+          } else {
+            // console.log('Successfully connected')
+            this.$session.set('name', log.data.name)
+            this.name = this.$session.get('name')
+            this.alertmsg = ' has been successfully connected'
+            alert(this.name + this.alertmsg)
+            this.$session.start()
+            // console.log(this.$session.id())
+            this.$router.push('/Home')
+          }
+        }
+      }
+    },
+    async register () {
+      if (this.Password === '' || this.Name === '') {
+        // console.log('missing password or name')
+        this.alertmsg = 'Password or name is empty'
+        this.warning = true
+      } else {
+        // console.log('register request')
+        const reg = await this.axios.post('/api/register', {
+          name: this.Name
+        })
+        // console.log(reg.data.status)
+        if (!reg.data.status) {
+          // console.log('this name is already taken')
+          this.alertmsg = 'Name already taken'
+          this.warning = true
+        } else {
+          // console.log('New user created')
+          this.axios.post('/api/registered', {
+            name: this.Name,
+            password: this.Password
+          })
+          this.alertmsg = 'Successfully created'
+          this.success = true
+          this.Name = ''
+          this.Password = ''
+        }
+      }
+    }
+  }
+}
+</script>
